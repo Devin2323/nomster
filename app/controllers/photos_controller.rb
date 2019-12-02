8cd483f0496cbddb0b80ad47196831ab1954db32
Devin Anderson <treblemakerdevin@gmail.com>
2019-12-02 22:17:09 +0000

    routing

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,9 +2,14 @@ class PhotosController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @place = Place.find(params[:place_id])
+    @photo = Place.find(params[:place_id])
     @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+    @photo = Photo.new
   end
 
   private
