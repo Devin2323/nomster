b248acddd2226d87f484887de1774fb8bd69739b
Devin Anderson <treblemakerdevin@gmail.com>
2019-12-23 17:58:41 +0000

    emailing basics

---
+++ test/mailers/notifictation_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class NotifictationMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/mailers/previews/notifictation_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/notifictation_mailer
+class NotifictationMailerPreview < ActionMailer::Preview
+
+end
